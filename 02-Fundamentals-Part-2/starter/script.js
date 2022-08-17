2dfeb1a7e3b6bb376626c69f6ebdba924080cda0
Lefteris Giannaros <lefterisgiannaros@gmail.com>
2022-08-17 23:21:27 +0300

    part 2 started

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+// 33. FUNCTIONS
+// function fruitProcessor(apples, oranges){
+//     console.log(`Apples: ${apples} Oranges: ${oranges}`)
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
+
+// 34. FUNCTION DECLARATIONS VS EXPRESSIONS
+
+// Function declaration
+function calcAge1(currentYear, birthYear){
+    return currentYear - birthYear;
+}
+const age1 = calcAge1(2022,1993);
+console.log(age1);
+
+
+// Function expression
+const age2 = function (currentYear, birthYear){
+    return currentYear - birthYear;
+}
+
+console.log(age2(2022,1993));
+
+
+// Arrow expression
+const calcAge3 = birthYear => currentYear - birthYear;
